ea548a4d870dd4ec977b81277fb2b489df18b2c8
medina16 <89689393+medina16@users.noreply.github.com>
2022-11-17 14:16:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran Siswa Baru | SMK Coding</title>
+</head>
+
+<body>
+	<header>
+		<h1>Laundry</h1>
+		<h3>Laundry di Kabupaten Bogor</h3>
+	</header>
+
+	<h4>Menu</h4>
+	<nav>
+		<ul>
+			<li><a href="formpengguna.php">Daftar</a></li>
+			<li><a href="login.php">Login</a></li>
+		</ul>
+	</nav>
+
+
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if ($_GET['status'] == 'sukses'){
+				echo "Pendaftaran siswa baru berhasil!";
+			} else if ($_GET['status'] == 'gagal'){
+				echo "Pendaftaran gagal!";
+			} 
+		?>
+	</p>
+	<?php endif; ?>
+
+	</body>
+</html>
