5c79302880cd6b5cd756a8bf50efadb2b24058e2
medina16 <89689393+medina16@users.noreply.github.com>
2022-11-28 22:40:27 +0700

    Update page_pengguna.php
    ngebetulin query sama tabel tampilan list laundry
    tombol ulasan diganti sama tombol info, nanti kalo mau ngulas dari page_infolaundry.php aja

--- page_pengguna.php
+++ page_pengguna.php
@@ -52,47 +52,30 @@
 	<table border="1">
 	<tbody>
 		<?php
-		$querylau = pg_query("SELECT * FROM laundryan WHERE idlau=$idlau");
-		while($data_laundryan = pg_fetch_array($querylau)){
-			echo "<tr>";
-			echo "<td>Nama Usaha</td>";
-			echo "<td>".$data_laundryan['namaus']."</td>";
-			echo "</tr>";
-
-			echo "<tr>";
-			echo "<td>Alamat</td>";
-			echo "<td>".$data_laundryan['alamat']."</td>";
-			echo "</tr>";
-
-			echo "<tr>";
-			echo "<td>Kelurahan</td>";
-			echo "<td>".$data_laundryan['kelurahan']."</td>";
-			echo "</tr>";
-
-			echo "<tr>";
-			echo "<td>Kontak Telepon</td>";
-			echo "<td>".$data_laundryan['kontak']."</td>";
-			echo "</tr>";
-
-			echo "<tr>";
-			echo "<td>Tarif Laundry Kiloan Minimum</td>";
-			echo "<td>".$data_laundryan['tarif']."</td>";
-			echo "</tr>";
-
-			echo "<tr>";
-			echo "<td>Rincian Harga</td>";
-			echo "<td>".nl2br($data_laundryan['rincian'])."</td>";
-			echo "</tr>";
-			}
+            $querylau = pg_query("SELECT * FROM laundryan");
+            while($data_laundryan = pg_fetch_array($querylau)){
+                $penilaian = pg_query("SELECT ROUND(AVG(rating)::numeric,2) FROM ulasan WHERE idlau = $data_laundryan[idlau]");
+		        $berapa = pg_fetch_array($penilaian);
+                echo "<tr>";
+                echo "<td>
+                        </p>
+                        <b>".$data_laundryan['namaus']."</b>
+                        <p> <b>Rating: ".$berapa['round']."/5</b></br>
+                        <b>Kelurahan</b>           : ".$data_laundryan['kelurahan']."</br>
+                        <b>Tarif kiloan minimum</b>: ".$data_laundryan['tarif']."</br>
+                        <form action=page_infolaundry.php?idlau=".$data_laundryan['idlau']." method=post>
+                            <input type=hidden value=".$id." name=iduser />
+                            <input type=submit value='Lihat info' name=ingpo />
+                        </form>
+                        </p>
+                    </td>";
+                echo "</tr>";
+            }
 		?>
 	</tbody>
 	</table>
 	
 	</br>
-	<form action="formulasan.php" method="post">
-        <input type="hidden" value="<?=$idpem?>" name="idpem" />
-        <input type="submit" value="Beri Ulasan" name="ulasan" />
-    </form>
 
 	<?php if(isset($_GET['status'])): ?>
 	<p>
@@ -104,3 +87,7 @@
 			} 
 		?>
 	</p>
+	<?php endif; ?>
+
+	</body>
+</html>
