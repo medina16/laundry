8babd02693c9d536130cc22a2cfa4b64e206d71d
medina16 <89689393+medina16@users.noreply.github.com>
2022-11-28 16:01:47 +0700

    Update aksihapus_pemilik.php
    disesuain sama confirmhapus_pemilik.php

--- aksihapus_pemilik.php
+++ aksihapus_pemilik.php
@@ -1,19 +1,32 @@
 <?php
 include("configbaru.php");
 if( isset($_POST['hapuspemilik']) ){
-    $idpem = $_POST['idpem'];
-    $id_lau = $_POST['idlau'];
-    $idul = $_POST['idul']
 
-    $query1 = pg_query("DELETE FROM ulasan WHERE idul = $idul");
-    $query2 = pg_query("DELETE FROM laundryan WHERE idlau = $id_lau");
-    $query3 = pg_query("DELETE FROM pemilik WHERE idpem = $idpem");
+    $idpem = (int)$_POST['idpem'];
 
-    if( $query1 && $query2 && $query3 ){
-        header('Location: page_pemilik.php');
+    $loggedin_lau = pg_query("SELECT idlau FROM laundryan WHERE idpem = $idpem");
+	$cariid_lau = pg_fetch_array($loggedin_lau);
+	$idlau = $cariid_lau['idlau'];
+
+    $hapusulasan = pg_query("DELETE FROM ulasan WHERE idlau = $idlau");
+    $hapuslaundryan = pg_query("DELETE FROM laundryan WHERE idpem = $idpem");
+    $hapuspemilik = pg_query("DELETE FROM pemilik WHERE idpem = $idpem");
+
+    if( $hapusulasan && $hapuslaundryan && $hapuspemilik ){
+        header('Location: index.php?status_hapus=sukses');
     } else {
-        die("gagal menghapus...");
+        header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."&status_hapus=gagal");
     }
+
+} else if(isset($_POST['gadeng'])){
+
+    $idpem = (int)$_POST['idpem'];
+    
+    $loggedin_lau = pg_query("SELECT idlau FROM laundryan WHERE idpem = $idpem");
+	$cariid_lau = pg_fetch_array($loggedin_lau);
+	$idlau = $cariid_lau['idlau'];
+
+    header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."");
 
 } else {
     die("akses dilarang...");
