894ef557a62907bc3d5a48bf13a38a423f9b0c84
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:33:43 +0700

    Update daftar_pengguna.php

--- daftar_pengguna.php
+++ daftar_pengguna.php
@@ -1,35 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Formulir Pendaftaran Siswa Baru | SMK Coding</title>
+	<title>Laundry - Daftar Sebagai Pengguna</title>
+	<link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body>
 	<header>
-		<h3>Pendaftaran Pengguna</h3>
+		<div class="container">
+			<h2>Pendaftaran Pengguna</h2>
+		</div>
 	</header>
-
+	
+	<div class="daptar">
 	<form action="aksidaftar_pengguna.php" method="POST">
 		<fieldset>
-
 		<p>
-			<label for="email">E-mail: </label>
-			<input type="text" name="email" placeholder="alamat e-mail" />
+			<label for="email">E-mail</label></br>
+			<input type="text" name="email" placeholder="alamat e-mail" maxlength="255" required/>
 		</p>
 		<p>
-			<label for="password">Kata Sandi: </label>
-			<input type="password" name="password" placeholder="password" />
+			<label for="password">Kata Sandi</label></br>
+			<input type="password" name="password" placeholder="password" maxlength="16" required/>
 		</p>
 		<p>
-			<label for="nama">Nama Pengguna: </label>
-			<input type="text" name="nama" placeholder="nama lengkap" />
+			<label for="nama">Nama Pengguna</label></br>
+			<input type="text" name="nama" placeholder="nama lengkap" maxlength="64" required/>
 		</p>
 			<input type="submit" value="Daftar" name="daftar" />
+			<a class="button-putih" href="
+					<?php if (isset($_SERVER['HTTP_REFERER'])) {
+						echo "$_SERVER[HTTP_REFERER]";
+					} else {
+						echo "index.php";}?>
+			">Batal
+			</a>
 		</p>
-		
-
 		</fieldset>
 	</form>
+	</div>
 
 	</body>
 </html>
