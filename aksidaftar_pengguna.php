c98ab70e8f91bc82abd015898c8546b33d4af33c
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:18:34 +0700

    Update aksidaftar_pengguna.php

--- aksidaftar_pengguna.php
+++ aksidaftar_pengguna.php
@@ -1,28 +1,33 @@
 <?php
 include("configbaru.php");
 
-// cek apakah tombol daftar sudah diklik atau blum?
+// cek apakah tombol 'daftar' sudah diklik
 if(isset($_POST['daftar'])){
+	session_start();
+	session_destroy();
 
-	$nama = $_POST['nama'];
-	$email = $_POST['email'];
-	$pw = $_POST['password'];
+	if(($_POST['nama']!=NULL) && ($_POST['email']!=NULL) && ($_POST['password']!=NULL)){
+		//ambil data pengguna
+		$nama = $_POST['nama'];
+		$email = $_POST['email'];
+		$pw = $_POST['password'];
+		$querypengguna = pg_query("INSERT INTO pengguna (email, password, nama) VALUES ('$email','$pw','$nama')");
+	}
 
-
-  	$querypengguna = pg_query("INSERT INTO pengguna (email, password, nama) VALUES ('$email','$pw','$nama')");
+	//buat record baru di database pemilik, lalu ambil id pengguna
 	$ambiliduser = pg_query("SELECT iduser, email FROM pengguna WHERE email = '$email'");
 	$ambiliduser2 = pg_fetch_array($ambiliduser);
-    	$iduser = $ambiliduser2['iduser'];
+    $iduser = $ambiliduser2['iduser'];
 
-
-	// apakah query berhasil?
+	//apakah query berhasil?
     if( $querypengguna){
-		header("Location: page_pengguna.php?iduser=".$iduser."&status_daftar=sukses");
+		//jika berhasil, mulai session, set iduser untuk session, alihkan ke page_pengguna.php
+		session_start();
+		$_SESSION["iduser"] = $iduser;
+		header("Location: page_pengguna.php?status_daftar=sukses");
     } else {
-		header("Location: index.php?status_daftar=gagal");
+		header("Location: gagaldaftar.php");
     }
-
-
 
 } else {
 	die("Akses dilarang...");
