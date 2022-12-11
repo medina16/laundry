ea9f4f924ac21225f32453e6fcc6f78823fe74e7
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:32:08 +0700

    Update confirmhapus_pemilik.php

--- confirmhapus_pemilik.php
+++ confirmhapus_pemilik.php
@@ -1,30 +1,42 @@
 <?php
 include("configbaru.php");
-if( isset($_POST['hapuspemilik']) ){
-    $idpem = (int)$_POST['idpem'];
-} else {
-    die("akses dilarang...");
-}
+session_start();
+	if($_SESSION['idpem']!=NULL && $_SESSION['idlau']!=NULL){
+		$idpem = $_SESSION['idpem']; 
+	} else {
+		header("Location:page_login.php");
+	}
 ?>
 
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <title>Laundry - Konfirmasi</title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body>
-
-    <form action="aksihapus_pemilik.php" method="POST">
+<header>
+		<div class=container> <h2>Konfirmasi</h2> </div>
+</header>
+<div class="daptar">
+<form action="aksihapus_pemilik.php" method="POST">
         <fieldset>
             <h2>Apakah anda yakin ingin menghapus akun?</h2>
             <p>Semua data terkait data Anda sebagai pemilik usaha laundry dan data informasi usaha Anda akan dihapus dari sistem. Anda juga akan ter-log out secara otomatis.</p>
             <input type="hidden" value="<?=$idpem?>" name="idpem" />
             <input type="submit" value="Ya" name="hapuspemilik" />
-            <input type="submit" value="Tidak" name="gadeng" />
+            <a class="button-putih" href="
+					<?php if (isset($_SERVER['HTTP_REFERER'])) {
+						echo "$_SERVER[HTTP_REFERER]";
+					} else {
+						echo "page_pemilik.php";}?>
+			">Tidak
+			</a>
         </fieldset>
     </form>
+</div>
 
 	</body>
 </html>
