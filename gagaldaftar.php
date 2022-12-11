9a1113c2bdfaa5590f65ecb02e8b5973ba48913c
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:36:05 +0700

    Create gagaldaftar.php

---
+++ gagaldaftar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laundry - Pendaftaran Gagal</title>
+	<link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+            <h2>Pendaftaran Gagal</h2>
+        </div>
+    </header>
+
+		<div class="konten">
+        <div class="aksi-gagal">
+			<p>Terjadi kesalahan saat pendaftaran.</p>
+            <a class="button-putih" href="daftar_pengguna.php">Daftar ulang sebagai pengguna</a>
+            <a class="button-putih" href="daftar_pemilik.php">Daftar ulang sebagai pemilik</a>
+            <a class="button-putih" href="index.php">Kembali ke halaman utama</a>
+        </div>
+
+</div>
+
+	</body>
+</html>
