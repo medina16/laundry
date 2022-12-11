a5e350c14890b402c09c08d296b6a3263c1a4d45
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:19:28 +0700

    Update aksiedit_pemilik.php

--- aksiedit_pemilik.php
+++ aksiedit_pemilik.php
@@ -1,10 +1,10 @@
 <?php
 include("configbaru.php");
 
-// cek apakah tombol edit sudah diklik atau blum?
+// cek apakah tombol 'edit' sudah diklik
 if(isset($_POST['edit'])){
 
-	// ambil id dari query string
+	//ambil data pemilik dari form pengeditan
 	$idpem = $_POST['idpem'];
 	$nama = $_POST['nama'];
 	$email = $_POST['email'];
@@ -12,6 +12,7 @@ if(isset($_POST['edit'])){
 	$jk = $_POST['jenis_kelamin'];
 	$notelp = $_POST['kontak'];
 
+	//ambil data usaha laundry dari form pengeditan
 	$idlau = $_POST['idlau'];
 	$namaus = $_POST['lname'];
 	$alamat = $_POST['alamat'];
@@ -20,17 +21,15 @@ if(isset($_POST['edit'])){
 	$rincian = $_POST['deskripsi'];
 	$kontakk = $_POST['kontakk'];
 
-	var_dump($_POST);
-
-	// buat query pengeditan
+	//lakukan update pada record di database pemilik dan laundryan sesuai data dari form
   	$query_pemilik = pg_query("UPDATE pemilik SET nama = '$nama', email = '$email', password = '$pw', sex = '$jk', kontak = '$notelp' WHERE idpem=$idpem");
 	$query_laundryan = pg_query("UPDATE laundryan SET namaus = '$namaus', alamat = '$alamat', kelurahan = '$kelurahan', tarif = '$tarif', kontak = '$kontakk', rincian = '$rincian' WHERE idlau=$idlau");
 
 	// apakah query berhasil?
     if( $query_pemilik && $query_laundryan){
-		header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."&status_edit=sukses");
+		header("Location: page_pemilik.php?status_edit=sukses");
     } else {
-		header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."&status_edit=gagal");
+		header("Location: page_pemilik.php?status_edit=gagal");
     }
 
 } else {
