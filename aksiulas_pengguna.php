2b088affe5f609f89f0d110c05c6b8d40f365da6
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:31:19 +0700

    Update and rename aksiulas_pelanggan.php to aksiulas_pengguna.php

--- aksiulas_pengguna.php
+++ aksiulas_pengguna.php
@@ -1,23 +1,23 @@
 <?php
 include("configbaru.php");
 
-// cek apakah tombol edit sudah diklik atau blum?
+// cek apakah tombol submit sudah diklik
 if(isset($_POST['submit'])){
 
-	var_dump($_POST);
+	//ambil data ulasan dari form pengeditan
 	$idlau =  $_POST['idlau'];
 	$iduser =  $_POST['iduser'];
 	$rating =  $_POST['rating'];
 	$review =  $_POST['review'];
 	
+	//buat record baru di database ulasan sesuai data dari form
 	$queryulasan = pg_query("INSERT INTO ulasan(idlau,iduser,rating,review,tanggal) VALUES($idlau,$iduser,'$rating','$review',localtimestamp(0));");
 	
 	// apakah query berhasil?
     if( $queryulasan ){
         header("Location: page_infolaundry.php?idlau=".$idlau."&status_ulas=sukses");
     } else {
-        //header('Location: daftarsiswa.php?status_edit=gagal');
-		printf('aw men');
+		header("Location: page_infolaundry.php?idlau=".$idlau."&status_ulas=gagal");
     }
 
 } else {
