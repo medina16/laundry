e4fe5c1ec7e5e8b7ed9fd4ad6af6560155737787
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:29:19 +0700

    Create aksilogin_pemilik.php

--- aksilogin_pemilik.php
+++ aksilogin_pemilik.php
@@ -1,27 +1,34 @@
 <?php
 include("configbaru.php");
-// cek apakah tombol daftar sudah diklik atau blum?
+// cek apakah tombol 'Login_pemilik' sudah diklik
 if(isset($_POST['Login_pemilik'])){
+	session_start();
+	session_destroy();
 
-	// ambil data dari formulir
+	// ambil data login pemilik
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 
-	// buat query
+	// lihat apakah pasangan email dan password terdapat pada database dan mengambil idpem
   	$loggedin_pem = pg_query("SELECT email, password, idpem FROM pemilik WHERE email='$email' AND password='$password'");
 	$cariid_pem = pg_fetch_array($loggedin_pem);
 	$idpem = $cariid_pem['idpem'];
 
 	// apakah pasangan email dan password terdapat dalam database?
 	if(pg_num_rows($loggedin_pem) == 0) {
-		// kalau gagal alihkan ke halaman loginp.php dengan cek_owner=salah
+		// kalau tidak, alihkan ke halaman loginp.php dengan cek_owner=salah
 		header('Location: page_login.php?cek_owner=salah');
 	} else {
-		// kalau berhasil alihkan ke halaman pemilikk.php
+		// kalau ada, ambil idlau dari usaha laundry yang dimiliki pemilik
 		$loggedin_lau = pg_query("SELECT idlau FROM laundryan WHERE idpem = $idpem");
 		$cariid_lau = pg_fetch_array($loggedin_lau);
 		$idlau = $cariid_lau['idlau'];
-		header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."");
+
+		//lalu mulai session, set idpem dan idlau, alihkan ke halaman page_pemilik.php
+		session_start();
+		$_SESSION["idpem"] = $idpem;
+		$_SESSION["idlau"] = $idlau;
+		header("Location: page_pemilik.php");
 	}
 
 } else {
