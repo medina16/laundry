ea1262b7bf89d67ae222947f34a088aacbe3f1dd
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:27:34 +0700

    Update aksihapus_pemilik.php

--- aksihapus_pemilik.php
+++ aksihapus_pemilik.php
@@ -13,20 +13,12 @@ if( isset($_POST['hapuspemilik']) ){
     $hapuspemilik = pg_query("DELETE FROM pemilik WHERE idpem = $idpem");
 
     if( $hapusulasan && $hapuslaundryan && $hapuspemilik ){
+        session_start();
+        session_destroy();
         header('Location: index.php?status_hapus=sukses');
     } else {
-        header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."&status_hapus=gagal");
+        header("Location: page_pemilik.php?status_hapus=gagal");
     }
-
-} else if(isset($_POST['gadeng'])){
-
-    $idpem = (int)$_POST['idpem'];
-    
-    $loggedin_lau = pg_query("SELECT idlau FROM laundryan WHERE idpem = $idpem");
-	$cariid_lau = pg_fetch_array($loggedin_lau);
-	$idlau = $cariid_lau['idlau'];
-
-    header("Location: page_pemilik.php?idpem=".$idpem."&idlau=".$idlau."");
 
 } else {
     die("akses dilarang...");
