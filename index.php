334dddc6de6638c51f3ad46dea40f72140c7e622
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-11 15:36:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pendaftaran Siswa Baru | SMK Coding</title>
+	<title>Laundry - Halaman Utama</title>
+	<link rel="stylesheet" href="stylesheet_index.css">
 </head>
 
 <body>
-	<header>
-		<h1>Laundry</h1>
-		<h3>Laundry di Kabupaten Bogor</h3>
-	</header>
-
-	<h4>Daftar</h4>
+		<div class="konten">
+			<div class="intro">
+				<h1>Laundry</h1>
+				<p>Daftarkan usaha laundry Anda atau cari dan beri ulasan untuk laundry-laundry di Kabupaten Bogor!</p>
+		</div>
 	<nav>
-		<ul>
-			<li><a href="daftar_pengguna.php">Daftar sebagai pengguna</a></li>
-			<li><a href="daftar_pemilik.php">Daftar sebagai pemilik</a></li>
-		</ul>
+		<a class="ijo" href="daftar_pengguna.php">Daftar sebagai pengguna</a>
+		<a class="ijo" href="daftar_pemilik.php">Daftar sebagai pemilik</a>
+		<a class="putih" href="page_login.php">Login</a>
 	</nav>
 
-	<?php if(isset($_GET['status_daftar'])): ?>
-	<p>
-		<?php
-			if ($_GET['status_daftar'] == 'gagal'){
-				echo "Pendaftaran gagal!";
-			} 
-		?>
-	</p>
-	<?php endif; ?>
-
-	<h4>Login</h4>
-	<nav>
-		<ul>
-			<li><a href="page_login.php">Login</a></li>
-		</ul>
-	</nav>
+</div>
 
 	</body>
 </html>
