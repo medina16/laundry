8793734ff6ab2da3f023c5c2cbd233ed32039755
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-18 14:47:38 +0700

    Update page_login.php

--- page_login.php
+++ page_login.php
@@ -12,6 +12,7 @@
 <head>
 	<title>Laundry - Login</title>
 	<link rel="stylesheet" href="stylesheet.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
 </head>
 
 <body>
