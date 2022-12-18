dce817ec1d2624e7171a93dacc3be474f622d1e2
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-18 16:56:33 +0700

    Update daftar_pemilik.php

--- daftar_pemilik.php
+++ daftar_pemilik.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Laundry - Daftar Sebagai Pemilik Usaha</title>
+	<title>BogorBinatu - Daftar Sebagai Pemilik Usaha</title>
 	<link rel="stylesheet" href="stylesheet.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
 </head>
