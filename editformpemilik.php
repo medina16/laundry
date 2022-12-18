deaae7e5bd08a335a6f9188e6cef7bb61e762979
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-18 16:55:47 +0700

    Update editformpemilik.php

--- editformpemilik.php
+++ editformpemilik.php
@@ -22,7 +22,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Laundry - Edit Data</title>
+	<title>BogorBinatu - Edit Data</title>
 	<link rel="stylesheet" href="stylesheet.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
 </head>
