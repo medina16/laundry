b90bd2b72fd7d384d2a3e1c3d971daadc0bcf480
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-18 15:16:18 +0700

    Update editformpengguna.php

--- editformpengguna.php
+++ editformpengguna.php
@@ -16,39 +16,47 @@ include ("configbaru.php");
 <!DOCTYPE html>
 <html>
 <head>
-<title>Laundry - Edit Data</title>
+	<title>Laundry - Edit Data</title>
+	<link rel="stylesheet" href="stylesheet.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
 </head>
 
 <body>
 	<header>
-		<h3>Pengeditan Data</h3>
+		<div class="container">
+		<h2>Pengeditan Data</h2>
+		</div>
 	</header>
-
+	
+	<div class="daptar">
 	<form action="aksiedit_pengguna.php" method="POST">
 		<fieldset>
-
 		<h4>Data Pengguna</h4>
 		<input type="hidden" name="iduser" value="<?=$iduser?>"/>
 		<p>
-			<label for="email">E-mail: </label>
-			<input type="text" name="email" placeholder="alamat e-mail" value="<?=$isiquery_pengguna['email']?>"/>
+			<label for="email">E-mail</label></br>
+			<input type="text" name="email" placeholder="alamat e-mail" maxlength="255" required value="<?=$isiquery_pengguna['email']?>"/>
 		</p>
 		<p>
-			<label for="password">Kata Sandi: </label>
-			<input type="password" name="password" placeholder="password" value="<?=$isiquery_pengguna['password']?>"/>
+			<label for="password">Kata Sandi</label></br>
+			<input type="password" name="password" placeholder="password" maxlength="16" required value="<?=$isiquery_pengguna['password']?>"/>
 		</p>
-		
 		<p>
-			<label for="nama">Nama Pengguna: </label>
-			<input type="text" name="nama" placeholder='nama lengkap' value="<?=$isiquery_pengguna['nama']?>" />
+			<label for="nama">Nama Pengguna</label></br>
+			<input type="text" name="nama" placeholder="nama lengkap" maxlength="64" required value="<?=$isiquery_pengguna['nama']?>"/>
 		</p>
-			
 		<p>
 			<input type="submit" value="Edit" name="edit" />
+			<a class="button-putih" href="
+					<?php if (isset($_SERVER['HTTP_REFERER'])) {
+						echo "$_SERVER[HTTP_REFERER]";
+					} else {
+						echo "page_pengguna.php";}?>">Batal
+			</a>
 		</p>
-		
 		</fieldset>
 	</form>
+	</div>
 
 	</body>
 </html>
