c106b087d183917373e884600d25edca50e92635
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-18 16:52:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Laundry - Halaman Utama</title>
+	<title>BogorBinatu - Index</title>
 	<link rel="stylesheet" href="stylesheet_index.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
 </head>
