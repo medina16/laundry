359534b94988169a69d16e815fb69d55c0c8ea44
medina16 <89689393+medina16@users.noreply.github.com>
2022-12-18 17:02:24 +0700

    Update configbaru.php

--- configbaru.php
+++ configbaru.php
@@ -1,5 +1,5 @@
 <?php
-$db=pg_connect('host=localhost dbname=proyekbasdat user=postgres password=\'16 Joolee 2003\'');
+$db=pg_connect('host=localhost dbname=infolaundry user=postgres password=\'16 Joolee 2003\'');
 if( !$db ){
     die("Gagal terhubung dengan database: " . pg_connect_error());
 }
